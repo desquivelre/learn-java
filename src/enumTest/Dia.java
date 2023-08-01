c5296c868c180e405215fc174da84618564e0306
Diego Esquivel <desquivelreuni@hotmail.com>
2023-08-01 14:59:56 -0500

    Se creo el package testEnum

---
+++ src/enumTest/Dia.java
@@ -0,0 +1,5 @@
+package enumTest;
+
+public enum Dia {
+    LUNES, MARTES, MIERCOLES, JUEVES, VIERNES, SABADO, DOMINGO
+}
---
+++ src/enumTest/testDia.java
@@ -0,0 +1,8 @@
+package enumTest;
+
+public class testDia {
+    public static void main(String[] args) {
+
+
+    }
+}
