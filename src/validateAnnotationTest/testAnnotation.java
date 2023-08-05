9f8a54a41124991611aea650365d755d321abf43
Diego Esquivel <desquivelreuni@hotmail.com>
2023-08-04 21:34:58 -0500

    Creacion de EdadMinimaUtil y testAnnotation

--- src/validateAnnotationTest/testAnnotation.java
+++ src/validateAnnotationTest/testAnnotation.java
@@ -3,10 +3,12 @@ package validateAnnotationTest;
 import java.time.LocalDate;
 import java.time.Month;
 
+import static validateAnnotationTest.EdadMinimaUtil.validador;
+
 public class testAnnotation {
     public static void main(String[] args) {
-        Usuario usuario = new Usuario("Diego", "contraseña123", LocalDate.of(1995, Month.MARCH, 14));
+        Usuario usuario = new Usuario("Diego", "contraseña123", LocalDate.of(2009, Month.MARCH, 14));
 
-//        System.out.println(validador(usuario));
+        System.out.println(validador(usuario));
     }
 }
